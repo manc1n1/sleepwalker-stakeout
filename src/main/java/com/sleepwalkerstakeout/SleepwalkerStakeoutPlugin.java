9483d9ee6351af04ff1b974b8760e3a01a55ca82
manc1n1 <mancinij1111@gmail.com>
2026-08-23 23:32:56 -0400

    feat: add Blisterwood stake fake XP drop overlay

---
+++ src/main/java/com/sleepwalkerstakeout/SleepwalkerStakeoutPlugin.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2026, manc1n1 https://github.com/manc1n1
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sleepwalkerstakeout;
+
+import javax.inject.Inject;
+
+import net.runelite.api.Client;
+import net.runelite.api.EquipmentInventorySlot;
+import net.runelite.api.Item;
+import net.runelite.api.ItemContainer;
+import net.runelite.api.events.AnimationChanged;
+import net.runelite.api.gameval.InventoryID;
+import net.runelite.api.gameval.ItemID;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+@PluginDescriptor(
+        name = "Sleepwalker Stakeout",
+        description = "Displays a Blisterwood stake sprite as a fake XP drop when one is thrown",
+        tags = {"phosani", "nightmare", "sleepwalker", "blisterwood", "pnm", "bossing", "xp", "drop"}
+)
+public class SleepwalkerStakeoutPlugin extends Plugin {
+    private static final int BLISTERWOOD_STAKE_THROW_ANIMATION = 7617;
+
+    @Inject
+    private Client client;
+
+    @Inject
+    private ClientThread clientThread;
+
+    @Inject
+    private OverlayManager overlayManager;
+
+    @Inject
+    private SleepwalkerStakeoutOverlay overlay;
+
+    @Override
+    protected void startUp() {
+        overlayManager.add(overlay);
+        clientThread.invokeLater(overlay::prepareSprite);
+    }
+
+    @Override
+    protected void shutDown() {
+        overlayManager.remove(overlay);
+        overlay.clear();
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onAnimationChanged(AnimationChanged event) {
+        if (event.getActor() != client.getLocalPlayer()
+                || event.getActor().getAnimation() != BLISTERWOOD_STAKE_THROW_ANIMATION
+                || !isBlisterwoodStakeEquipped()) {
+            return;
+        }
+
+        overlay.addDrop();
+    }
+
+    private boolean isBlisterwoodStakeEquipped() {
+        final ItemContainer equipment = client.getItemContainer(InventoryID.WORN);
+
+        if (equipment == null) {
+            return false;
+        }
+
+        final Item weapon = equipment.getItem(
+                EquipmentInventorySlot.WEAPON.getSlotIdx()
+        );
+
+        return weapon != null && weapon.getId() == ItemID.BLISTERWOOD_STAKE;
+    }
+}
